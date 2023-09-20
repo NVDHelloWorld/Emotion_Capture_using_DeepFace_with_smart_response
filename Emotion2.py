7be31e386dbfd585e2d10e05bdd26f4315f94523
NVDHelloWorld <119719194+NVDHelloWorld@users.noreply.github.com>
2023-09-20 20:00:34 +0530

    Add files via upload
    This Python script is a Streamlit web application that allows users to perform real-time emotion detection using a webcam, upload an image from their system for emotion detection, or capture an image from the webcam and analyze the emotion detected in the image. Here's a description of the main features and functionality:
    
    Emotion Detection Options:
    
    The application provides three options in the sidebar for users to choose from:
    "Real-time Emotion Detection": Analyzes emotions in real-time using the webcam.
    "Capture Image from System": Allows users to upload an image from their system for emotion analysis.
    "Click Image": Captures an image using the webcam and analyzes the detected emotion.
    Real-time Emotion Detection:
    
    When the "Real-time Emotion Detection" option is selected, the webcam is activated, and the application continuously captures video frames.
    It processes each frame to detect the emotion of the face in the frame using a pre-trained deep learning model (DEEPFACE).
    The detected emotion is displayed in real-time along with a rectangle drawn around the face and a caption.
    Capture Image from System:
    
    Users can upload an image (in formats such as JPG, JPEG, or PNG) from their local system using the file uploader.
    The uploaded image is processed, and the detected emotion is displayed, along with the uploaded image.
    Based on the detected emotion, a corresponding response or message is displayed.
    Click Image:
    
    By clicking the "Click Image" button, users can capture an image using their webcam.
    Similar to the "Capture Image from System" option, the detected emotion is displayed along with the captured image.
    A response or message based on the detected emotion is also provided.
    Emotion and Response:
    
    The application recognizes emotions such as "happy," "sad," "angry," "neutral," "surprise," "disgust," and "fear."
    For each detected emotion, a predefined response or message is displayed to provide a user-friendly interaction.
    Text-to-Speech:
    
    The application utilizes the pyttsx3 library to convert the response or message into speech for a more interactive experience.
    User Interaction:
    
    Users can choose their preferred option from the sidebar, upload images, and interact with the application through button clicks.
    Graceful Exit:
    
    For the real-time emotion detection option, users can press 'q' to exit the webcam video feed and close the application.
    Overall, this Streamlit application offers an engaging and interactive way for users to explore real-time emotion detection and image analysis using pre-trained deep learning models while providing responses and messages based on the detected emotions.

---
+++ Emotion2.py
@@ -0,0 +1,186 @@
+import cv2
+import numpy as np
+from deepface import DeepFace
+import pyjokes
+import random
+import pyttsx3
+import streamlit as st
+
+# Load DEEPFACE model
+model = DeepFace.build_model('Emotion')
+
+# Define emotion labels
+emotion_labels = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
+
+# Function to predict emotion
+def predict_emotion(frame):
+    # Resize frame
+    resized_frame = cv2.resize(frame, (48, 48), interpolation=cv2.INTER_AREA)
+
+    # Convert frame to grayscale
+    gray_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
+
+    # Preprocess the image for DEEPFACE
+    img = gray_frame.astype('float32') / 255.0
+    img = np.expand_dims(img, axis=-1)
+    img = np.expand_dims(img, axis=0)
+
+    # Predict emotions using DEEPFACE
+    preds = model.predict(img)
+    emotion_idx = np.argmax(preds)
+    emotion = emotion_labels[emotion_idx]
+
+    return emotion
+
+# Streamlit app
+def main():
+    st.title("Emotion Detection App")
+    st.write("Capture an image from your webcam and detect your emotion!")
+
+    # Add a sidebar for options
+    option = st.sidebar.radio("Select an option", ("Real-time Emotion Detection", "Capture Image from System", "Click Image"))
+
+    if option == "Real-time Emotion Detection":
+        st.write("Real-time Emotion Detection is active. Press 'q' to exit.")
+        
+        # Start capturing video
+        cap = cv2.VideoCapture(0)
+
+        # Initialize image_container with a default image
+        default_image = np.zeros((300, 400, 3), dtype=np.uint8)
+        image_container = st.image(default_image, caption="Detected Emotion:", use_column_width=True)
+
+        while True:
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+
+            # Resize frame
+            resized_frame = cv2.resize(frame, (48, 48), interpolation=cv2.INTER_AREA)
+
+            # Convert frame to grayscale
+            gray_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
+
+            # Preprocess the image for DEEPFACE
+            img = gray_frame.astype('float32') / 255.0
+            img = np.expand_dims(img, axis=-1)
+            img = np.expand_dims(img, axis=0)
+
+            # Predict emotions using DEEPFACE
+            preds = model.predict(img)
+            emotion_idx = np.argmax(preds)
+            emotion = emotion_labels[emotion_idx]
+
+            # Draw rectangle around face and label with predicted emotion
+            cv2.rectangle(frame, (0, 0), (200, 30), (0, 0, 0), -1)
+            cv2.putText(frame, emotion, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+            # Display the resulting frame in Streamlit
+            image_container.image(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), caption=f"Detected Emotion: {emotion}", use_column_width=True)
+
+            # Press 'q' to exit
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # Release the capture and close all windows
+        cap.release()
+        cv2.destroyAllWindows()
+    elif option == "Capture Image from System":
+        # Capture an image from the user's system and predict emotion
+        uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+        if uploaded_image is not None:
+            # Read the uploaded image
+            image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)
+
+            # Predict emotion
+            emotion = predict_emotion(image)
+
+            # Convert BGR image to RGB for colorful display
+            rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+            # Display the uploaded image with the predicted emotion
+            st.image(rgb_image, caption=f"Detected Emotion: {emotion}", use_column_width=True)
+
+            # Define responses based on emotions
+            responses = {
+                'happy': "HEY! You look happy. Congrats!",
+                'sad': pyjokes.get_joke(),
+                'angry': random.choice([
+                    "Take a deep breath and count to ten.",
+                    "Stay calm and carry on.",
+                    "Inhale the future, exhale the past.",
+                    "Keep calm and let it go.",
+                ]),
+                'neutral': "You seem to be in a neutral mood.",
+                'surprise': "You look surprised!",
+                'disgust': "You seem disgusted.",
+                'fear': "You appear to be fearful."
+            }
+
+            # Display the response based on the detected emotion
+            if emotion in responses:
+                response_text = responses[emotion]
+                st.write(f"**Emotion:** {emotion}")
+                st.write(f"**Response:** {response_text}")
+
+                # Initialize the text-to-speech engine
+                engine = pyttsx3.init()
+
+                # Speak the response
+                engine.say(response_text)
+                engine.runAndWait()
+            else:
+                st.warning("Emotion not recognized.")
+
+    elif option == "Click Image":
+        # Capture an image using the webcam and predict emotion
+        if st.button("Click Image"):
+            # Capture an image using the webcam
+            cap = cv2.VideoCapture(0)
+            ret, frame = cap.read()
+
+            # Predict emotion
+            emotion = predict_emotion(frame)
+
+            # Convert BGR image to RGB for colorful display
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # Display the captured image with the predicted emotion
+            st.image(rgb_frame, caption=f"Detected Emotion: {emotion}", use_column_width=True)
+
+            # Define responses based on emotions
+            responses = {
+                'happy': "HEY! You look happy. Congrats!",
+                'sad': pyjokes.get_joke(),
+                'angry': random.choice([
+                    "Take a deep breath and count to ten.",
+                    "Stay calm and carry on.",
+                    "Inhale the future, exhale the past.",
+                    "Keep calm and let it go.",
+                ]),
+                'neutral': "You seem to be in a neutral mood.",
+                'surprise': "You look surprised!",
+                'disgust': "You seem disgusted.",
+                'fear': "You appear to be fearful."
+            }
+
+            # Display the response based on the detected emotion
+            if emotion in responses:
+                response_text = responses[emotion]
+                st.write(f"**Emotion:** {emotion}")
+                st.write(f"**Response:** {response_text}")
+
+                # Initialize the text-to-speech engine
+                engine = pyttsx3.init()
+
+                # Speak the response
+                engine.say(response_text)
+                engine.runAndWait()
+            else:
+                st.warning("Emotion not recognized.")
+
+            # Release the capture
+            cap.release()
+
+if __name__ == "__main__":
+    main()
